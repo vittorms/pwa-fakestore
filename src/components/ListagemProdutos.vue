c27f40d838b2427dafc4ad73d31e9bbd56cf0fb7
vittorms <vitor.msalomao@gmail.com>
2024-03-05 16:56:02 -0300

    criação composables screean

--- src/components/ListagemProdutos.vue
+++ src/components/ListagemProdutos.vue
@@ -1,6 +1,9 @@
 <script setup>
 import { ref, onMounted } from 'vue';
 import axios from 'axios';
+import { useScreen } from '@/composables/screen';
+
+const { browserWidth, deviceWidth, isMobile } = useScreen();
 const produtos = ref([]);
 
 onMounted(async () => {
@@ -13,7 +16,11 @@ const formatPrice = (price) => `R$ ${price.toFixed(2).replace('.', ',')}`;
 
 <template>
   <div>
-    <h1>Produtos</h1>
+    <h1>
+       Produtos - {{ browserWidth }} - {{ deviceWidth }} - {{
+      isMobile}} 
+      <span v-if="isMobile">É móvel</span>
+    </h1>
     <div class="container">
       <div class="card" v-for="produto in produtos" :key="produto.id">
         <h1 class="card--title">{{ produto.title }}</h1>
